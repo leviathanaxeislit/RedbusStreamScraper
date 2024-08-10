cad86d92c954cdd3683e9ec59d8cdc83fc731ef5
habro <aliazthomaz@gmail.com>
2024-08-10 14:19:28 +0530

    Database connection addded

--- admin.py
+++ admin.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from src.data_extraction.scrapping_utils import WebScrap
-RED_BUS_URL = "https://www.redbus.in/online-booking/rtc-directory"
+from src.redbus_constants import RED_BUS_URL
 scrap_obj = WebScrap(RED_BUS_URL)
 scrap_obj.extract_all_rtc_operators()
+df = pd.DataFrame(scrap_obj.bus_list)
+df.to_excel("rtc_list.xlsx")
 print(scrap_obj.bus_list)
--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-from src.data_extraction.scrapping_utils import WebScrap
-from admin import RED_BUS_URL
-scrap_obj = WebScrap(RED_BUS_URL)
-scrap_obj.extract_all_rtc_operators()
-bus_routes  = [item.get("Text") for item in scrap_obj.bus_list]
+import pandas as pd
+df = pd.read_excel('rtc_list.xlsx')
+bus_routes = df['Text']
 st.title("RedBus")
 option = st.selectbox(
     'Choose youre Bus Route:',
